77d497991a2068806bd607f9dcbb61baaf545b42
Michael Williams <manicdesigner@gmail.com>
2019-06-08 13:33:14 -0400

    feat(Starwars..list.js) map out StarwarsCharacter components

--- starwars/src/components/StarwarsCharList.js
+++ starwars/src/components/StarwarsCharList.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './StarWars.css';
+import StarwarsCharacter from './StarwarsCharacter';
 
 const StarwarsCharList = props => {
     return (
-        <div>
-            {}
+        <div className={"character-display"}>
+            {props.starwarsCharacters.map( (character) => {
+                return <StarwarsCharacter
+                    key={character.created}
+                    character={character}
+                    characterDisplay={props.characterDisplay}
+                    />
+                }
+            )
+        }
         </div>
     );
 }
