407d742018f012eef22255669980288ab0f2ce8d
Michael Williams <manicdesigner@gmail.com>
2019-06-08 12:34:23 -0400

    mockup(Charact...er.js) + build out component

---
+++ starwars/src/components/CharacterContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './StarWars.css';
+
+const CharacterContainer = () => {
+    return (
+        <div>
+            
+        </div>
+    ); 
+}
+export default CharacterContainer;
